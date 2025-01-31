7fe2d68d5f4f7ef54081d470c7502318a0ab4619
Ajinkya-Shetty10 <amazingaji10@gmail.com>
2025-01-31 14:07:31 -0500

    Added parse transport function for TCP UDP AND ICMP

--- pktsniffer.py
+++ pktsniffer.py
@@ -1,5 +1,6 @@
 import argparse
 from scapy.all import rdpcap, Ether, IP, TCP, UDP, ICMP
+from ipaddress import ip_network, ip_address
 
 def parse_arguments():
     """Parses command-line arguments using argparse."""
@@ -23,6 +24,19 @@ def parse_ip(pkt):
     """Parses IP header."""
     ip = pkt[IP]
     return ip.version, ip.ihl, ip.tos, ip.len, ip.id, ip.flags, ip.frag, ip.ttl, ip.proto, ip.chksum, ip.src, ip.dst
+
+def parse_transport(pkt):
+    """Parses TCP, UDP, or ICMP headers."""
+    if TCP in pkt:
+        tcp = pkt[TCP]
+        return f"TCP: Src Port={tcp.sport}, Dst Port={tcp.dport}, Flags={tcp.flags}"
+    elif UDP in pkt:
+        udp = pkt[UDP]
+        return f"UDP: Src Port={udp.sport}, Dst Port={udp.dport}"
+    elif ICMP in pkt:
+        icmp = pkt[ICMP]
+        return f"ICMP: Type={icmp.type}, Code={icmp.code}, Checksum={icmp.chksum}"
+    return "Other Protocol"
 
 if __name__ == "__main__":
     args = parse_arguments()
